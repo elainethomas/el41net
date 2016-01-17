549e4598148c7d9ebd4261b7afe1b85502430a01
Elaine Thomas <enajera2@cnm.edu>
2016-01-17 15:54:50 -0700

    adding content to page framework

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -7,17 +7,19 @@
 	<body>
 		<h1>Purpose</h1>
 		<p>
-			My personal website project will be my first impression for potential employers.  On it's face it demonstrate It should showcase my past projects programming abilities
+			My personal website project will be my first impression for potential employers.  On it's face it will showcase a sample of my front end design abilities.  It will also provide links to my coding portfolio and information about my education and certifications.  Lastly, my availability and contact information will be provided at the bottom.  Eventually, clients would come to my site to hire me for their projects.
 		</p>
-
 		<h1>Audience</h1>
 		<p>
 			This site is designed for a two groups - employers who want to hire junior web developers and other programmers looking for a freelance programmer or business partner.
 		</p>
 
 		<h1>Goal</h1>
-		<p>
-			Words on a screen
-		</p>
+		<ul>
+			<li>Obtain a full or part time job using my coding skills.</li>
+			<li>Create a partnership with other programmers to work on a coding project.</li>
+			<li>Get hired for freelance or contract coding work.</li>
+			<li>Act as my "home" on the web to showcase my work and highlight projects I've been a part of.</li>
+		</ul>
 	</body>
 </html>
