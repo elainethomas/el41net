899920739fe8568045bf9af866eaeb5fe9a38364
Elaine Thomas <enajera2@cnm.edu>
2016-01-17 22:13:23 -0700

    adding content to page framework

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -7,24 +7,62 @@
 	<body>
 		<h3>Purpose</h3>
 		<p>
-			My personal website project will be my first impression for potential employers.  On it's face it will showcase a sample of my front end design abilities.  It will also provide links to my coding portfolio and information about my education and certifications.  Lastly, my availability and contact information will be provided at the bottom.  Eventually, clients would come to my site to hire me for their projects.
+			My personal website project will be my first impression for potential employers.  On it's face it will showcase a sample of my front end design abilities.  It will also provide links to my coding portfolio and information about my education and certifications.  Lastly, my availability and contact information will also be detailed.  Eventually, clients would come to my site to hire me for their projects.
 		</p>
 		<h3>Audience</h3>
 		<p>
-			This site is designed for three groups of people - employers who want to hire junior web developers, other programmers looking for a freelance programmer or business partner and customers looking for a coder with skills.
+			This site is designed for three groups of people - employers who want to hire junior web developers, other developers looking for a freelance coder or business partner and customers looking for a web developer with skills.
 		</p>
 
 		<h3>Goal</h3>
 		<ul>
-			<li>Obtain a full or part time job using my coding skills.</li>
-			<li>Create a partnership with other programmers to work on a coding project.</li>
+			<li>Obtain a full or part time job using my web development skills.</li>
+			<li>Create a partnership with other developers to work on a coding project.</li>
 			<li>Get hired for freelance or contract coding work.</li>
 			<li>Act as my "home" on the web to showcase my work and highlight projects I've been a part of.</li>
 		</ul>
 		<hr>
-		<h2>Persona</h2>
-		<img src="BobDobbs-color.png" />
-		<h3 class="persona">"</h3>
-		<h4>Bob, small business owner, 60 yrs. old</h4>
+		<h2>Bob, Local Business Owner - SubG Stone, 56 yrs. old</h2>
+		<img src="BobDobbs-color.png" width="125" height="150"/>
+		<h3>"I was obliged to be industrious. Whoever is equally industrious will succeed equally well."</h3>
+		<p>
+			Bob is the owner of a local stone working company.  His company's website hasn't been substantially updated in almost ten years.  Searching and ordering capabilities will open up new revenue streams.  Bob has built his business from the ground up since the late 1970s and has amassed a large customer database.  SubG Stone has weekly specials on a variety of products.  Currently, customers cannot order stone from the SubG website; customers can fill out a contact form to request information.  Bob believes his customers appreciate his no-nonsense way of doing business and will also appreciate a no-nonsense kind of website.
+		</p>
+		<h3>Needs</h3>
+		<ul>
+			<li>See the web developer's existing portfolio.</li>
+			<li>Check what programming languages the developer knows.</li>
+			<li>See what education the developer has.</li>
+		</ul>
+		<br>
+		<h3>Frustrations</h3>
+		<p>
+			Because of Bob's concern about losing customers to the big box stores with strong SEO and modern sites he has been compelled to hire a web developer to update his site.  As owner of SubG Stone, Bob likes to be involved in the vetting of prospective employees.  The process is time consuming though so he has the IT manager sift through the applicant's technical qualifications and then send the best candidate's websites to Bob via email.  Bob doesn't know much about coding but he knows a good website when he sees it.  Bob is not inclined to spend a lot of time searching around a programmer's website to see if he will like their work.
+		</p>
+
+		<h3>Ideal</h3>
+		<ul>
+			<li>Bob would like to see the applicant's work with no more than two clicks from the landing page.</li>
+			<li>If Bob likes what he sees he wants to find the applicant's contact information quickly.</li>
+			<li>Bob doesn't like overly fussy websites.</li>
+			<li>Bob wants the company website to be clear and to the point.</li>
+		</ul>
+		<hr>
+		<h3>Use Case</h3>
+		<h4>Basic Flow</h4>
+		<ul>
+			<li>Navigate to portfolio link
+				<ol>
+					<li>click through</li>
+					<li>use nav bar to got contacts</li>
+					<li>contact me to discuss a new project</li>
+				</ol>
+			</li>
+			<li>stuff</li>
+			<li>stuff</li>
+		</ul>
 	</body>
 </html>
+
+
+<!--detail the time, location, technology used, and the purpose or goal of your Persona's use of your site.-->
