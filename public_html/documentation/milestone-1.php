2e3e4ea733368d5b99363e90a733d041b7c6e986
Elaine Thomas <enajera2@cnm.edu>
2016-01-17 17:16:42 -0700

    adding content to page framework

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -5,21 +5,26 @@
 		<title>Milestone 1</title>
 	</head>
 	<body>
-		<h1>Purpose</h1>
+		<h3>Purpose</h3>
 		<p>
 			My personal website project will be my first impression for potential employers.  On it's face it will showcase a sample of my front end design abilities.  It will also provide links to my coding portfolio and information about my education and certifications.  Lastly, my availability and contact information will be provided at the bottom.  Eventually, clients would come to my site to hire me for their projects.
 		</p>
-		<h1>Audience</h1>
+		<h3>Audience</h3>
 		<p>
-			This site is designed for a two groups - employers who want to hire junior web developers and other programmers looking for a freelance programmer or business partner.
+			This site is designed for three groups of people - employers who want to hire junior web developers, other programmers looking for a freelance programmer or business partner and customers looking for a coder with skills.
 		</p>
 
-		<h1>Goal</h1>
+		<h3>Goal</h3>
 		<ul>
 			<li>Obtain a full or part time job using my coding skills.</li>
 			<li>Create a partnership with other programmers to work on a coding project.</li>
 			<li>Get hired for freelance or contract coding work.</li>
 			<li>Act as my "home" on the web to showcase my work and highlight projects I've been a part of.</li>
 		</ul>
+		<hr>
+		<h2>Persona</h2>
+		<img src="BobDobbs-color.png" />
+		<h3 class="persona">"</h3>
+		<h4>Bob, small business owner, 60 yrs. old</h4>
 	</body>
 </html>
