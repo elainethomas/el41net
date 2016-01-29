4912c8d3f58dceebaedba71c746e3c1ce9123aa8
Elaine Thomas <enajera2@cnm.edu>
2016-01-29 12:20:36 -0700

    attempt to show txt file in phase2a

--- public_html/documentation/milestone-2.php/phase2a.php
+++ public_html/documentation/milestone-2.php/phase2a.php
@@ -1,6 +1,15 @@
-<?php
-$desktopWireframe = "desktopWireframe.txt";
-if(file_exists($desktopWireframe)){
-	echo file_get_contents($desktopWireframe);
-}
-?>
+<!DOCTYPE html>
+<html lang="en">
+	<meta charset="utf-8" />
+	<head>
+		<title>Phase2a Requirements</title>
+	</head>
+	<body>
+		<h2>Site Map & Wireframes</h2>
+		<object>
+			<iframe src="desktopWireframe.txt" frameborder="0" height="400" width="95%">
+
+			</iframe>
+		</object>
+	</body>
+</html>
