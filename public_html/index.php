82913aa89301f196cd52e8ebe7a37e64e654ccac
Elaine Thomas <enajera2@cnm.edu>
2016-02-02 21:15:01 -0700

    removing unwanted code from nav bar

--- public_html/index.php
+++ public_html/index.php
@@ -50,9 +50,10 @@
 
 						<!-- Collect the nav links, forms, and other content for toggling -->
 						<div class="collapse navbar-collapse" id="top-nav">
-							<ul class="nav navbar-nav">
+							<ul class="nav navbar-nav navbar-right">
 								<li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
 								<li><a href="#">Link</a></li>
+								<!--I don't want this here
 								<li class="dropdown">
 									<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
 									<ul class="dropdown-menu">
@@ -66,13 +67,15 @@
 									</ul>
 								</li>
 							</ul>
+							<!--I don't want this here
 							<form class="navbar-form navbar-left" role="search">
 								<div class="form-group">
 									<input type="text" class="form-control" placeholder="Search">
 								</div>
 								<button type="submit" class="btn btn-default">Submit</button>
 							</form>
-							<ul class="nav navbar-nav navbar-right">
+							-->
+
 								<li><a href="#">Link</a></li>
 								<li class="dropdown">
 									<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
@@ -80,8 +83,8 @@
 										<li><a href="#">Action</a></li>
 										<li><a href="#">Another action</a></li>
 										<li><a href="#">Something else here</a></li>
-										<li role="separator" class="divider"></li>
-										<li><a href="#">Separated link</a></li>
+										<!--<li role="separator" class="divider"></li>
+										<li><a href="#">Separated link</a></li>-->
 									</ul>
 								</li>
 							</ul>
