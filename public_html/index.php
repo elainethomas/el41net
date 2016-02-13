888995049e60b1dc03c653bd2ca421e63efbd375
Elaine Thomas <enajera2@cnm.edu>
2016-02-13 16:49:35 -0700

    adjusting colors

--- public_html/index.php
+++ public_html/index.php
@@ -31,22 +31,24 @@
 
 		<title>el41net</title>
 	</head>
-	<body class="bodClass">
-		<div class="mainContent">
+	<body>
+		<div>
 			<header class="page-header">
 			<!--	<h1>Example page header <small>Subtext for header</small></h1> -->
 				<div class="container">
+					<div class="bg"></div>
 					<div class="jumbotron">
 						<h1>&lt;!--   Infinite Possibilities   --></h1>
-						<h3>Elaine Thomas</h3>
-						<h4>Web Developer</h4>
-						<p></p>
+						<h2>Elaine Thomas</h2>
+						<h3>Web Developer</h3>
+						<p>design, code, implement, maintain</p>
 					</div>
 				<div class="container">
 				<nav class="navbar navbar-default"> <!--use navbar-inverse for darker bg-->
 					<div class="container-fluid">
 						<!-- Brand and toggle get grouped for better mobile display -->
 						<div class="navbar-header">
+							<h3>Quality you can see.</h3>
 							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#top-nav" aria-expanded="false">
 								<span class="sr-only">Toggle navigation</span>
 								<span class="icon-bar"></span>
@@ -103,33 +105,66 @@
 			</header>
 				<main >
 					<div class="container">
-						<div class="row">
-							<div class="col-md-12">
-								<div class="content-box">
-									Hello
+
+							<div class="container">
+								<br>
+								<div id="myCarousel" class="carousel slide" data-ride="carousel">
+									<!-- Indicators -->
+									<ol class="carousel-indicators">
+										<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+										<li data-target="#myCarousel" data-slide-to="1"></li>
+										<li data-target="#myCarousel" data-slide-to="2"></li>
+										<li data-target="#myCarousel" data-slide-to="3"></li>
+									</ol>
+
+									<!-- Wrapper for slides -->
+									<div class="carousel-inner" role="listbox">
+
+										<div class="item active">
+											<img src="http://placehold.it/460x345">
+											<div class="carousel-caption">
+												<h3>Chania</h3>
+												<p>The atmosphere in Chania has a touch of Florence and Venice.</p>
+											</div>
+										</div>
+
+										<div class="item">
+											<img src="http://placehold.it/460x345">
+											<div class="carousel-caption">
+												<h3>Chania</h3>
+												<a href="#">The atmosphere in Chania has a touch of Florence and Venice.</a>
+											</div>
+										</div>
+
+										<div class="item">
+
+											<img src="http://placehold.it/460x345">
+											<div class="carousel-caption">
+												<h3>Flowers</h3>
+												<p>Beatiful flowers in Kolymbari, Crete.</p>
+											</div>
+										</div>
+
+										<div class="item">
+											<img src="http://placehold.it/460x345">
+											<div class="carousel-caption">
+												<h3>Flowers</h3>
+												<p>Beatiful flowers in Kolymbari, Crete.</p>
+											</div>
+										</div>
+
+									</div>
+
+									<!-- Left and right controls -->
+									<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+										<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+										<span class="sr-only">Previous</span>
+									</a>
+									<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+										<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+										<span class="sr-only">Next</span>
+									</a>
 								</div>
-							</div>
-						</div>
-						<div class="row">
-							<div class="col-md-6">
-								words
-							</div>
-							<div class="col-md-6">
-								here
-							</div>
-						</div>
-						<div class="row">
-							<div class="col-md-3">
-								last row 1
-							</div>
-							<div class="col-md-3">
-								last row 2
-							</div>
-							<div class="col-md-3">
-								last row 3
-							</div>
-							<div class="col-md-3">
-								last row 4
 							</div>
 
 						</div>
