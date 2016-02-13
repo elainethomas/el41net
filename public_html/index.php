b1799347d2464788e3dfa4f2d0147e23c1780d58
Elaine Thomas <enajera2@cnm.edu>
2016-02-13 13:47:59 -0700

    added jumbro tron banner, adjusting nav bar

--- public_html/index.php
+++ public_html/index.php
@@ -33,8 +33,15 @@
 	</head>
 	<body class="bodClass">
 		<div class="mainContent">
-			<header div class="page-header">
-				<h1>Example page header <small>Subtext for header</small></h1>
+			<header class="page-header">
+			<!--	<h1>Example page header <small>Subtext for header</small></h1> -->
+				<div class="container">
+					<div class="jumbotron">
+						<h1>&lt;!--   Infinite Possibilities   --></h1>
+						<h3>Elaine Thomas</h3>
+						<h4>Web Developer</h4>
+						<p></p>
+					</div>
 				<div class="container">
 				<nav class="navbar navbar-default"> <!--use navbar-inverse for darker bg-->
 					<div class="container-fluid">
@@ -46,15 +53,15 @@
 								<span class="icon-bar"></span>
 								<span class="icon-bar"></span>
 							</button>
-							<!--<a class="navbar-brand" href="#">Brand</a> remove this line, add <img src="blah.jpg" alt="blah">-->
-							<img src="http://placehold.it/50x50">
+							<!--<a class="navbar-brand" href="#">Brand</a> remove this line, add <img src="blah.jpg" alt="blah">
+							<img src="http://placehold.it/50x50">-->
 						</div>
 
 						<!-- Collect the nav links, forms, and other content for toggling -->
 						<div class="collapse navbar-collapse" id="top-nav">
 							<ul class="nav navbar-nav navbar-right">
-								<li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
-								<li><a href="#">Link</a></li>
+								<li class="active"><a href="#">Link 1<span class="sr-only">(current)</span></a></li>
+								<li><a href="#">Link 2</a></li>
 								<!--I don't want this here
 								<li class="dropdown">
 									<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
@@ -78,7 +85,7 @@
 							</form>
 							-->
 
-								<li><a href="#">Link</a></li>
+								<li><a href="#">Link 3</a></li>
 								<!--
 								<li class="dropdown">
 									<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
@@ -94,7 +101,7 @@
 					</div><!-- /.container-fluid -->
 				</nav>
 			</header>
-				<main>
+				<main >
 					<div class="container">
 						<div class="row">
 							<div class="col-md-12">
