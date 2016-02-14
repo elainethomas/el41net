1ee012004448e3f08aec927049cbe6053e7324aa
Elaine Thomas <enajera2@cnm.edu>
2016-02-13 21:46:02 -0700

    working with background

--- public_html/index.php
+++ public_html/index.php
@@ -18,7 +18,7 @@
 				crossorigin="anonymous"/>
 
 		<!-- LOAD OUR CUSTOM STYLESHEET HERE!!!!! -->
-		<link rel="stylesheet" href="style.css"/>
+		<link rel="stylesheet" href="el41netStyle.css"/>
 
 		<!-- HTML5 shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
 		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -35,104 +35,114 @@
 				  integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS"
 				  crossorigin="anonymous"></script>
 
-		<title>el41net</title>
+		<title>EL41NET</title>
 	</head>
 	<body>
-			<header class="page-header">
-
-					<nav class="navbar navbar-default"> <!--use navbar-inverse for darker bg-->
-
-						<div class="container-fluid">
-							<!-- Brand and toggle get grouped for better mobile display -->
-							<div class="navbar-header">
-								<h3></h3>
-								<button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
-										  data-target="#top-nav" aria-expanded="false">
-									<span class="sr-only">Toggle navigation</span>
-									<span class="icon-bar"></span>
-									<span class="icon-bar"></span>
-									<span class="icon-bar"></span>
-								</button>
-							</div>
-							<!-- Collect the nav links, forms, and other content for toggling -->
-							<div class="collapse navbar-collapse" id="top-nav">
-								<ul class="nav navbar-nav navbar-right">
-									<li><a href="#">Hello<span class="sr-only">(current)</span></a></li>
-									<li><a href="#">Portfolio</a></li>
-									<li><a href="#">About Me</a></li>
-									<li><a href="#">Let's Talk</a></li>
-								</ul>
-							</div><!-- /.navbar-collapse -->
-						</div><!-- /.container-fluid -->
-					</nav>
-					<div class="bg"></div>
-					<div class="jumbotron">
-						<h1>&lt;!-- Infinite Possibilities --></h1>
-						<h2>Elaine Thomas</h2>
-						<h3>Web Developer</h3>
-						<p>design, code, implement, maintain</p>
+		<header class="page-header">
+			<div class="container">
+				<div class="bg"></div>
+				<div class="jumbotron">
+					<h1>&lt;!-- Infinite Possibilities --></h1>
+					<h2>Elaine Thomas</h2>
+					<h3>Web Developer</h3>
+					<p>design, code, implement, maintain</p>
+				</div>
+				<nav class="navbar navbar-inverse"> <!--use navbar-default for lighter bg-->
+					<!-- Brand and toggle get grouped for better mobile display -->
+					<div class="navbar-header">
+						<h3></h3>
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+								  data-target="#top-nav" aria-expanded="false">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
 					</div>
-			</header>
-			<main>
-					<div class="container">
-						<br>
-						<div id="myCarousel" class="carousel slide" data-ride="carousel">
-							<!-- Indicators -->
-							<ol class="carousel-indicators">
-								<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-								<li data-target="#myCarousel" data-slide-to="1"></li>
-								<li data-target="#myCarousel" data-slide-to="2"></li>
-								<li data-target="#myCarousel" data-slide-to="3"></li>
-							</ol>
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse" id="top-nav">
+						<ul class="nav navbar-nav navbar-right">
+							<li><a href="#">Hello<span class="sr-only">(current)</span></a></li>
+							<li><a href="#">Portfolio</a></li>
+							<li><a href="#">About Me</a></li>
+							<li><a href="#">Let's Talk</a></li>
+						</ul>
+					</div>
+			</div>
+		</header>
+		<div class="container">
 
-							<!-- Wrapper for slides -->
-							<div class="carousel-inner" role="listbox">
+		</div>
 
-								<div class="item active">
-									<img src="http://placehold.it/460x345">
-									<div class="carousel-caption">
-										<h3>Super Cool Persona</h3>
-										<a href="#">Design that works for you.</a>
-									</div>
-								</div>
 
-								<div class="item">
-									<img src="http://placehold.it/460x345">
-									<div class="carousel-caption">
-										<h3>That Website I designed</h3>
-										<a href="#">I can make stuff that looks cool.</a>
-									</div>
-								</div>
+		<main>
+			<div class="container">
+				<div class="row">
+					<div class="span4"></div>
+				<img src="images/moon.jpg" alt="reach for moon" class="banner">
+					<div class="span4"></div>
+				</div>
+			</div>
+			<div class="container">
+				<br>
+				<div id="myCarousel" class="carousel slide" data-ride="carousel">
+					<!-- Indicators -->
+					<ol class="carousel-indicators">
+						<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+						<li data-target="#myCarousel" data-slide-to="1"></li>
+						<li data-target="#myCarousel" data-slide-to="2"></li>
+						<li data-target="#myCarousel" data-slide-to="3"></li>
+					</ol>
 
-								<div class="item">
+					<!-- Wrapper for slides -->
+					<div class="carousel-inner" role="listbox">
 
-									<img src="http://placehold.it/460x345">
-									<div class="carousel-caption">
-										<h3>TimeCruncher</h3>
-										<a href="#">A Handy Scheduling App</a>
-									</div>
-								</div>
-
-								<div class="item">
-									<img src="images/siMonSiteLink.jpg" alt="Praeter Simon">
-									<div class="carousel-caption">
-										<h3>SiMonZon Nemesis Homage</h3>
-										<a href="https://bootcamp-coders.cnm.edu/~enajera2/simonzon/">All Your Bases Belongs to SiMonZon</a>
-									</div>
-								</div>
-
+						<div class="item active">
+							<img src="http://placehold.it/460x345">
+							<div class="carousel-caption">
+								<h3>Super Cool Persona</h3>
+								<a href="#">Design that works for you.</a>
 							</div>
-
-							<!-- Left and right controls -->
-							<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-								<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-								<span class="sr-only">Previous</span>
-							</a>
-							<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-								<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-								<span class="sr-only">Next</span>
-							</a>
 						</div>
+
+						<div class="item">
+							<img src="http://placehold.it/460x345">
+							<div class="carousel-caption">
+								<h3>That Website I designed</h3>
+								<a href="#">I can make stuff that looks cool.</a>
+							</div>
+						</div>
+
+						<div class="item">
+
+							<img src="http://placehold.it/460x345">
+							<div class="carousel-caption">
+								<h3>TimeCruncher</h3>
+								<a href="#">A Handy Scheduling App</a>
+							</div>
+						</div>
+
+						<div class="item">
+							<img src="http://placehold.it/460x345">
+							<div class="carousel-caption">
+								<h3>SiMonZon Nemesis Homage</h3>
+								<a href="https://bootcamp-coders.cnm.edu/~enajera2/simonzon/">All Your Bases Belongs to
+									SiMonZon</a>
+							</div>
+						</div>
+
 					</div>
+
+					<!-- Left and right controls -->
+					<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+						<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+						<span class="sr-only">Previous</span>
+					</a>
+					<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+						<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+						<span class="sr-only">Next</span>
+					</a>
+				</div>
+			</div>
 	</body>
 </html>
